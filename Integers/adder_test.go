58d99d800c86e7d7ba0e347de0aa2e735e46c4fe
Артём Зараев <artemzaraev@MacBook-Air-Artem.local>
2025-03-09 22:13:36 +0300

    Добавлен вторая тема суммирования целых чисел

---
+++ Integers/adder_test.go
@@ -0,0 +1,19 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdder(t *testing.T) {
+	sum := Add(2, 2)
+	expected := 4
+	if sum != expected {
+		t.Errorf("Сумма некоректна, получили: %d, хотели: %d.", sum, expected)
+	}
+}
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
+}
